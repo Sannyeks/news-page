6813db7e1c672ed7d9f470885f94f1762a74a7da
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-03-05 16:35:11 +0200

    favorite-new
    favorite-new

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,102 +1,49 @@
-import axios from 'axios';
-import { cardItems } from './cardMarkup';
 
-const undefinedReadeMore = document.querySelector('.underfined');
-const block = document.querySelector('.cards__list');
-console.log(localStorage.setItem(' block', "cards__list"));
-const newList = document.querySelector('.cards__list');
+import cardMarkup from "./cardMarkup";
+const favoriteContainer = document.querySelector('.favorite-container');
+const favoriteNotFound = document.querySelector('.favorite-not-found');
 
 
-newList.addEventListener('click', removeToFavorite);
-const dataInLocal = JSON.parse(localStorage.getItem('favorite'));
+const FAVORITE_KEY = "favorite-key";
 
-if (dataInLocal === null) {
-  undefinedReadeMore.classList.remove('underfined-hidden');
-}
-function removeToFavorite(e) {
-  const btn = e.target.closest(`.item-news__remove-to-favorite-btn`);
-  if (!btn) return;
-  if (!dataInLocal) {
-    return;
-  }
-  console.log(btn.parentNode.parentNode.parentNode.parentNode);
-  let uri =
-    btn.parentNode.parentNode.nextElementSibling.nextElementSibling
-      .lastElementChild.textContent;
+const favoriteNews = localStorage.getItem(FAVORITE_KEY);
+let parseFavoriteNews = JSON.parse(favoriteNews);
 
-  for (let i = 0; i < dataInLocal.length; i += 1) {
-    if (dataInLocal[i].uri === uri) {
-      dataInLocal.splice(i, 1);
+
+favoriteContainer.innerHTML = cardMarkup(parseFavoriteNews);
+
+
+favoriteContainer.addEventListener('click', removeFromFavorite);
+function removeFromFavorite(event) {
+  console.log(event.target.dataset.url);
+  let currentFavorite = event.target.dataset.url;
+  parseFavoriteNews.map(favoriteCard => {
+    if (favoriteCard.web_url === currentFavorite) {
+      const favoriteCardIndex = parseFavoriteNews.indexOf(favoriteCard)
+ 
+      parseFavoriteNews.splice(favoriteCardIndex, 1);
+      localStorage.setItem(FAVORITE_KEY, JSON.stringify(parseFavoriteNews));
+      favoriteContainer.innerHTML = cardMarkup(parseFavoriteNews);
     }
+  })
+  
+}
+
+
+function favoriteNotFoundShow() {
+  console.log(parseFavoriteNews === []);
+  if (parseFavoriteNews.length === 0) {
+    console.log(1111);
+    favoriteContainer.style.display = 'none';
+    favoriteNotFound.style.display = 'flex';
+    favoriteNotFound
+      .querySelector('.underfined')
+      .classList.remove('underfined-hidden');
+
   }
-  localStorage.setItem(`favorite`, JSON.stringify(dataInLocal));
-  btn.parentNode.parentNode.parentNode.parentNode.remove();
-  // location.reload();
 }
 
-function getLocalData() {
-  if (localStorage.getItem('favorite') === null) return;
-  if (JSON.parse(localStorage.getItem('favorite')).length === 0) {
-    console.log('error');
-    undefinedReadeMore.classList.remove('underfined-hidden');
-    return;
-  }
-  const data = JSON.parse(localStorage.getItem('favorite'));
-  const markup = createMarkup(data);
-  block.insertAdjacentHTML('beforeend', markup);
-}
-
-getLocalData();
+favoriteNotFoundShow();
 
 
-const galleryContainer = document.querySelector('.gallery container');
-const itemsMarkup = document.querySelector('.cardItems');
 
-function createGallery(items) {
-    return items.map({ headline, web_url, pub_date, lead_paragraph, news_desk, bigMobileImg, smallMobileImg, smallSquareImg, bigSquareImg } => {
-        const lenght = lead_paragraph.length > 80
-      ? lead_paragraph.slice(0, 80) + '...'
-      : lead_paragraph;
-        return `
-         <li class="card__item">
-        <div class="card__wrapper">
-        <div class="card-image__wrapper">
-         <img
-          class="card__image"
-          src="https://www.nytimes.com/${bigSquareImg}"
-          alt="news"
-          width="288"
-          height="395"
-          loading='lazy'
-        />
-        <p class="card-jobsearching"> ${news_desk} </p>
-        <button class="card__btn-add hidden-span" type="button">Add to favorite 
-        <span class="card__btn-add">Add to favorite<svg class=""></svg></span>
-         <svg class=""></svg>
-        <span class="card__btn-remove">Add to favorite<svg class=""></svg></span>
-         ::after
-        </button>
-        <span class="card-already__read is-hidden">Already read</span>
-         <span class="card-already__read is-hidden">Already read</span>
-        <button class="card__btn" type="button">Add to favorite</button>
-      </div>
-
-      <h2 class="card__title">${headline}</h2>
-      <p class="card__article">${lenght}</p>
-
-      <div class="card__info">
-        <span class="card__info--date">${pub_date}</span>
-        <a class="card__info--readmore" href="${web_url}" target="_blank">
-          Read more
-        </a>
-      </div>
-    </div>
-  </li>`
-    }).join(''); 
-}
-
-galleryContainer.innerHTML = itemsMarkup;
-
-document.querySelector('.news-loader__container ').classList.add('is-hidden');
-
-export { createGallery, block };
