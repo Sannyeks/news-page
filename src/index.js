47afc0c40c3a18cd9d8ba737730d0153de5a1c37
Daria-Misko <miskodaria.mail@gmail.com>
2023-03-10 16:14:42 +0200

    work with dark theme

--- src/index.js
+++ src/index.js
@@ -19,3 +19,32 @@ mobileSwitcher.onclick = function(){
 .classList.toggle("dark-theme")
 }
 
+// document.body.classList.toggle('light-theme')
+
+// const switcher = document.getElementById('switch-style')
+// const mobileSwitcher = document.getElementById('switch-style2')
+// const mobContainer = document.querySelector('.modal-menu__conteiner')
+
+// function saveThemeToLocalStorage(theme) {
+//   localStorage.setItem('selectedTheme', theme);
+// }
+
+// switcher.onclick = function(){
+// 	const selectedTheme = document.body.classList.toggle("dark-theme")
+// 	document.body.classList.toggle("dark-theme")
+
+// 	saveThemeToLocalStorage(selectedTheme);
+// }
+// mobileSwitcher.onclick = function(){
+// 	const selectedTheme = mobContainer.classList.contains('dark-theme') ? 'light' : 'dark';
+// 	classList.toggle("dark-theme")
+
+// 	saveThemeToLocalStorage(selectedTheme);
+// }
+
+// window.addEventListener('load', function() {
+//   const selectedTheme = localStorage.getItem('selectedTheme');
+//   if (selectedTheme) {
+//     document.body.classList.add(selectedTheme + '-theme');
+//   }
+// });
