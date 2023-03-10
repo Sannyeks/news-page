a8ca8624f004650316942156de295e32b5be9205
Daria-Misko <miskodaria.mail@gmail.com>
2023-03-10 15:57:18 +0200

    change color theme

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@
 // ulEl.insertAdjacentHTML('beforeend', cardMarkup());
 
 const switcher = document.getElementById('switch-style')
+const mobileSwitcher = document.getElementById('switch-style2')
+const mobContainer = document.querySelector('.modal-menu__conteiner')
 
 switcher.onclick = function(){
 	document.body.classList.toggle("dark-theme")
 }
+mobileSwitcher.onclick = function(){
+	mobContainer = document.querySelector('.modal-menu__conteiner')
+.classList.toggle("dark-theme")
+}
+
